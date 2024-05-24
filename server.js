732fca2f8fdf1bdceee366e0c96f23a0bcd73193
pradeepkpanja <pradeepkpanja24@gmail.com>
2024-05-23 22:55:13 -0400

    Video UUID added with New User Console o/p

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app)
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidv4 } = require("uuid");
 
-app.get('/',(req,res)=>{
-    res.status(200).send('Hello Nodejs')
-})
+//set public files to be here
+app.use(express.static("public"));
 
-server.listen(3030)
+//set view engine ejs
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+//create a url
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected');
+  });
+});
+
+server.listen(3030);
