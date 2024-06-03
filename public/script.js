91e263b7a93e85028e50d6fd7916ae0ea3288917
pradeepkpanja <pradeepkpanja24@gmail.com>
2024-06-03 18:52:48 -0400

    "Bottom functionalities added with Chat App "

--- public/script.js
+++ public/script.js
@@ -52,3 +52,78 @@ const addVideoStream = (video, stream) => {
   });
   videoGrid.append(video);
 };
+
+let msg = $("input");
+console.log(msg);
+
+$("html").keydown((e) => {
+  if (e.which == 13 && msg.val().length !== 0) {
+    console.log(msg.val());
+    socket.emit("message", msg.val());
+    msg.val("");
+  }
+});
+
+socket.on("createMessage", (message) => {
+  $("ul").append(`<li class="message"><b>User</b></br>${message}</li></br>`);
+  scrollToBottom();
+});
+
+const scrollToBottom = () => {
+  let d = $(".main_chat_window");
+  d.scrollTop(d.prop("scrollHeight"));
+};
+
+//mute function
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    myVideoStream.getAudioTracks()[0].enabled = true;
+    setMuteButton();
+  }
+};
+
+const setMuteButton = () => {
+  const html = `<i class="fa-solid fa-microphone"></i>
+  <span>Mute</span>`;
+
+  document.querySelector(".main_mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `<i class="unmute fa-solid fa-microphone-slash"></i>
+  <span>Unmute</span>`;
+
+  document.querySelector(".main_mute_button").innerHTML = html;
+};
+
+// Play or stop the video stream
+
+const playStop = () => {
+  const enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    myVideoStream.getVideoTracks()[0].enabled = true;
+    setStopVideo();
+  }
+};
+
+const setPlayVideo = () => {
+  const html = `<i class="stop fa-solid fa-video-slash"></i>
+  <span>Play</span>`;
+
+  document.querySelector(".main_video_button").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `<i class=" fa-solid fa-video"></i>
+  <span>Stop Video</span>`;
+
+  document.querySelector(".main_video_button").innerHTML = html;
+};
